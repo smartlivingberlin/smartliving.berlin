30411403d07d17fc2358fd2b32a3a76e856f1926
alisadgadyri38 <alisadgadyri38@gmail.com>
2025-09-10 22:45:21 +0200

    feat: Crisp immer anzeigen (chat:show on load)

--- crisp.js
+++ crisp.js
@@ -1,4 +1,5 @@
 window.$crisp=[];window.CRISP_WEBSITE_ID="ce8f2b1d-c3f4-4bec-95ae-0a1f10f2f507";
+window.addEventListener("load",function(){ window.$crisp.push(["do","chat:show"]); });
 window.CRISP_RUNTIME_CONFIG = { locale: 'de' };
 (function(){
   var d=document,s=d.createElement("script");
