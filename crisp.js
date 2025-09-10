54427a39599b9ffdcbb988f179a4bb8d44712e56
alisadgadyri38 <alisadgadyri38@gmail.com>
2025-09-10 22:35:34 +0200

    chore: crisp.js mit sichtbarem onload-Log

--- crisp.js
+++ crisp.js
@@ -1,6 +1,8 @@
-window.=[];window.CRISP_WEBSITE_ID="ce8f2b1d-c3f4-4bec-95ae-0a11f0f25f07";
-(function(){var d=document,s=d.createElement("script");
-window.CRISP_RUNTIME_CONFIG = { locale: "de" };
+window.$crisp=[];window.CRISP_WEBSITE_ID="ce8f2b1d-c3f4-4bec-95ae-0a1f10f2f507";
 window.CRISP_RUNTIME_CONFIG = { locale: 'de' };
-s.src="https://client.crisp.chat/l.js";s.async=1;
-d.getElementsByTagName("head")[0].appendChild(s);})();
+(function(){
+  var d=document,s=d.createElement("script");
+  s.src="https://client.crisp.chat/l.js"; s.async=1;
+  s.onload=function(){ console.log("CRISP: l.js geladen ✔"); };
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
