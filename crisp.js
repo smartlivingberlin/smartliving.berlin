1e17af33bac74b6406c89f295fc3426114533d4c
alisadgadyri38 <alisadgadyri38@gmail.com>
2025-09-10 21:47:16 +0200

    chore: Crisp auf Deutsch (locale=de)

--- crisp.js
+++ crisp.js
@@ -1,4 +1,5 @@
 window.=[];window.CRISP_WEBSITE_ID="ce8f2b1d-c3f4-4bec-95ae-0a11f0f25f07";
 (function(){var d=document,s=d.createElement("script");
+window.CRISP_RUNTIME_CONFIG = { locale: "de" };
 s.src="https://client.crisp.chat/l.js";s.async=1;
 d.getElementsByTagName("head")[0].appendChild(s);})();
