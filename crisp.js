95bcbd50038ccbc1a743b44e2a83a1a7eeb8e212
alisadgadyri38 <alisadgadyri38@gmail.com>
2025-09-10 21:24:39 +0200

    chat: Crisp Widget eingebunden

--- crisp.js
+++ crisp.js
@@ -1,2 +1,4 @@
-window.$crisp=[];window.CRISP_WEBSITE_ID="DEINE-CRISP-ID";
-(function(){var d=document,s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+window.=[];window.CRISP_WEBSITE_ID="ce8f2b1d-c3f4-4bec-95ae-0a11f0f25f07";
+(function(){var d=document,s=d.createElement("script");
+s.src="https://client.crisp.chat/l.js";s.async=1;
+d.getElementsByTagName("head")[0].appendChild(s);})();
